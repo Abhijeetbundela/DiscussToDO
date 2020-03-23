dcba62755f5b07085ef59de21fa1b6e54fab5dd4
Abhijeet <abhijeetbundela26111997@gmail.com>
2020-03-24 02:19:33 +0530

    init

--- app/src/main/java/com/example/letsdiscusstodo/fragment/MyPostsFragment.java
+++ app/src/main/java/com/example/letsdiscusstodo/fragment/MyPostsFragment.java
@@ -1,7 +1,7 @@
 package com.example.letsdiscusstodo.fragment;
 
 
-import android.app.AlertDialog;
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -45,6 +45,7 @@ import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
